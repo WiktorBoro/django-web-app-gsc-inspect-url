de9ec9d7e4d1044b6df697f248e81f08196d1790
Wiktor Borowicz <105320026+WiktorBoro@users.noreply.github.com>
2022-06-01 23:34:47 +0200

    End 1.0

--- main/gsc_inspect_url/inspect_url.py
+++ main/gsc_inspect_url/inspect_url.py
@@ -5,37 +5,48 @@ import gspread
 from googleapiclient.discovery import build
 from celery import shared_task
 from re import match
+from json import loads
+
+from google.oauth2 import credentials
 
 
 @shared_task(bind=True)
-def start_inspect(token, sheets_url):
+def start_inspect(self, token, sheets_url):
     GscIndexInspect(token=token, sheets_url=sheets_url)
 
 
 class GscIndexInspect:
     def __init__(self, sheets_url, token):
         self.token = token
-        self.sheets_token = match('https:\/\/docs.google.com\/spreadsheets\/d\/([^\/]+)', sheets_url).groups()[0]
+        self.sheets_token = match('https:\/\/docs.google.com\/spreadsheets\/d\/([^\/]+)\/.*', sheets_url).groups()[0]
         self.authorization_gsc()
         self.authorization_sheets()
         self.get_url_from_sheets_and_save_to_sheets()
 
     def authorization_sheets(self):
-        credentials = CredentialsModel.objects.get(token='googlesheetscredNcQfTjWnZr4u7x')
+        credentials_sheets = sheets_cred
 
         gc = gspread.service_account_from_dict(credentials_sheets)
 
         self.sh = gc.open_by_key(self.sheets_token)
-        rows = self.sh.sheet1
+        self.rows = self.sh.sheet1
 
-        self.colA = rows.col_values(1)
+        self.colA = self.rows.col_values(1)
 
     def authorization_gsc(self):
-        credentials = CredentialsModel.objects.get(token=self.token)
-        http = httplib2.Http()
-        http = credentials.authorize(http)
+        credentials_dict = loads(CredentialsModel.objects.get(token=self.token).credential)
 
-        self.webmasters_service = build('searchconsole', 'v1', http=http)
+        credentials_ = credentials.Credentials(
+            credentials_dict['token'],
+            refresh_token=credentials_dict['refresh_token'],
+            id_token=credentials_dict['id_token'],
+            token_uri=credentials_dict['token_uri'],
+            client_id=credentials_dict['client_id'],
+            client_secret=credentials_dict['client_secret'],
+            scopes=[credentials_dict['scopes']],
+        )
+
+        self.webmasters_service = build('searchconsole', 'v1', credentials=credentials_)
 
     def index_inspect(self, url, site):
         request = {
@@ -47,21 +58,75 @@ class GscIndexInspect:
         return response
 
     def get_url_from_sheets_and_save_to_sheets(self):
-        row_num = 0
+        row_num = 1
+        self.rows.update_cell(row_num, 1, "URL")
+        self.rows.update_cell(row_num, 2, "Coverage state")
+        self.rows.update_cell(row_num, 3, "Indexing state")
+        self.rows.update_cell(row_num, 4, "Last crawl time")
+        self.rows.update_cell(row_num, 5, "Page fetch state")
+        self.rows.update_cell(row_num, 6, "Google canonical")
+        self.rows.update_cell(row_num, 7, "User canonical")
+        self.rows.update_cell(row_num, 8, "Crawled as")
+        self.rows.update_cell(row_num, 9, "Detected micro data")
+        self.rows.update_cell(row_num, 10, "Mobile usability result")
 
+        row_num = 2
         for url in self.colA:
             if match('^http.*', url):
-                domain = match(r'(https?:\/\/[^\/]+\/)', url)
-                response = self.index_inspect(url, domain.groups()[0])
-                coverage_state = response['inspectionResult']['indexStatusResult']['coverageState']
-                indexing_state = response['inspectionResult']['indexStatusResult']['indexingState']
+                try:
+                    domain = match(r'(https?:\/\/[^\/]+\/)', url)
+                    response = self.index_inspect(url, domain.groups()[0])
+                except:
+                    response = {'inspectionResult':
+                                    {'indexStatusResult': {'coverageState': 'ERROR', 'indexingState': 'ERROR'}}}
+                try:
+                    coverage_state = response['inspectionResult']['indexStatusResult']['coverageState']
+                except KeyError:
+                    coverage_state = "No data"
+                try:
+                    indexing_state = response['inspectionResult']['indexStatusResult']['indexingState']
+                except KeyError:
+                    indexing_state = "No data"
+                try:
+                    last_crawl_time = response['inspectionResult']['indexStatusResult']['lastCrawlTime']
+                except KeyError:
+                    last_crawl_time = "No data"
+                try:
+                    page_fetch_state = response['inspectionResult']['indexStatusResult']['pageFetchState']
+                except KeyError:
+                    page_fetch_state = "No data"
+                try:
+                    google_canonical = response['inspectionResult']['indexStatusResult']['googleCanonical']
+                except KeyError:
+                    google_canonical = "No data"
+                try:
+                    user_canonical = response['inspectionResult']['indexStatusResult']['userCanonical']
+                except KeyError:
+                    user_canonical = "No data"
+                try:
+                    crawled_as = response['inspectionResult']['indexStatusResult']['crawledAs'] + ' bot'
+                except KeyError:
+                    crawled_as = "No data"
+                try:
+                    detected_micro_data = ''
+                    for i in response['inspectionResult']['richResultsResult']['detectedItems']:
+                        detected_micro_data += str(i)+'\n'
+                except KeyError:
+                    detected_micro_data = "No data"
+
                 try:
                     mobile_usability_result = str(response['inspectionResult']['mobileUsabilityResult'])
                 except KeyError:
-                    mobile_usability_result = ""
+                    mobile_usability_result = "No data"
 
-                self.sh.update_cell(row_num, 2, coverage_state)
-                self.sh.update_cell(row_num, 3, indexing_state)
-                self.sh.update_cell(row_num, 4, mobile_usability_result)
+                self.rows.update_cell(row_num, 2, coverage_state)
+                self.rows.update_cell(row_num, 3, indexing_state)
+                self.rows.update_cell(row_num, 4, last_crawl_time)
+                self.rows.update_cell(row_num, 5, page_fetch_state)
+                self.rows.update_cell(row_num, 6, google_canonical)
+                self.rows.update_cell(row_num, 7, user_canonical)
+                self.rows.update_cell(row_num, 8, crawled_as)
+                self.rows.update_cell(row_num, 9, detected_micro_data)
+                self.rows.update_cell(row_num, 10, mobile_usability_result)
                 row_num += 1
         CredentialsModel.objects.filter(token=self.token).delete()
--- main/views_inspect_url.py
+++ main/views_inspect_url.py
@@ -1,48 +1,80 @@
 from django.shortcuts import render
 from .models import CredentialsModel
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.contrib.django_util.views import settings
-from oauth2client.contrib.django_util.storage import DjangoORMStorage
-from oauth2client.contrib import xsrfutil
+from django.http import HttpResponseRedirect, JsonResponse
+import google_auth_oauthlib.flow
 from secrets import token_urlsafe
-from .gsc_inspect_url.inspect_url import  start_inspect
-from json import loads
+from .gsc_inspect_url.inspect_url import start_inspect
+from json import dumps
+from datetime import datetime
+from django.urls import resolve
 
-FLOW = flow_from_clientsecrets(
-    settings.GOOGLE_OAUTH2_CLIENT_SECRETS_JSON,
-    scope=settings.GOOGLE_OAUTH2_SCOPE,
-    redirect_uri=settings.GOOGLE_OAUTH2_REDIRECT_URL)
+icea_flow = google_auth_oauthlib.flow.Flow.from_client_config(cred_icea,
+            scopes='https://www.googleapis.com/auth/webmasters.readonly')
+icea_redirect_uri = "https://robie-seo.herokuapp.com/inspect-gsc-icea/auth"
 
+normal_flow = google_auth_oauthlib.flow.Flow.from_client_config(cred,
+            scopes='https://www.googleapis.com/auth/webmasters.readonly')
+normal_redirect_uri = "https://robie-seo.herokuapp.com/inspect-gsc/auth"
 
 def inspect_gsc(response):
-    FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,
-                                                   response.user)
-    authorize_url = FLOW.step1_get_authorize_url()
-    return render(response, f'main/auth.html', {'authorize_url': authorize_url})
+
+    current_url = resolve(response.path_info).url_name
+
+    if 'inspect-gsc-icea' == current_url:
+        flow = icea_flow
+        flow.redirect_uri = icea_redirect_uri
+    else:
+        flow = normal_flow
+        flow.redirect_uri = normal_redirect_uri
+
+    authorization_url, _ = flow.authorization_url(
+        access_type='offline',
+        include_granted_scope='true',
+        prompt='consent')
+    return render(response, f'main/auth.html', {'authorize_url': authorization_url})
 
 
 def inspect_gsc_auth(response):
     token = token_urlsafe(nbytes=32)
-    if not xsrfutil.validate_token(settings.SECRET_KEY,
-                                   response.POST.get('state', response.GET.get('state')).encode('UTF-8'),
-                                   response.user):
-        return HttpResponseBadRequest()
-    credential = FLOW.step2_exchange(response.GET)
-    storage = DjangoORMStorage(CredentialsModel, 'token', token, 'credential')
-    storage.put(credential)
 
-    return HttpResponseRedirect(f'inspect-gsc/auth/{token}')
+    current_url = resolve(response.path_info).url_name
+
+    if 'inspect-gsc-icea-auth' == current_url:
+        flow = icea_flow
+        flow.redirect_uri = icea_redirect_uri
+    else:
+        flow = normal_flow
+        flow.redirect_uri = normal_redirect_uri
+
+    authorization_response = response.build_absolute_uri()
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = flow.credentials
+    credentials_dict = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'id_token': credentials.id_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes,
+        'expiry': datetime.strftime(credentials.expiry, '%Y-%m-%d %H:%M:%S')
+    }
+
+    CredentialsModel(token=token,
+                     credential=dumps(credentials_dict)).save()
+
+    return HttpResponseRedirect(f'/inspect-gsc/auth/{token}')
 
 
-def inspect_gsc_auth_start(response):
-    return render(response, f'main/auth.html', {})
+def inspect_gsc_auth_start(response, token):
+    return render(response, f'main/inspect_gsc_auth.html', {'token': token})
 
 
-def start_gsc_auth_start(response,):
+def auth_start_inspect(response):
     is_ajax = "robie-seo" in response.headers.get('host')
-    data = loads(response.body)
-    token = data['token']
-    sheets_url = data['sheets_url']
     if is_ajax and response.method == "POST":
+        token = response.POST['token']
+        sheets_url = response.POST['sheets_url']
         start_inspect.delay(token=token, sheets_url=sheets_url)
+        return JsonResponse({"Success": "success", "token": token, "sheets_url": sheets_url})
+    return JsonResponse({"Error": "error"})
