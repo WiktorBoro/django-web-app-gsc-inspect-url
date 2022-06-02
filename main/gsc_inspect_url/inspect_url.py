56fd0bb3d572ef5da040fc6c3e4f68a7af5f594c
Wiktor Borowicz <105320026+WiktorBoro@users.noreply.github.com>
2022-06-02 21:15:44 +0200

    fix no-response problem
    Added asynchronous to write cred to db
    Add googlesheets url and time to db models 
    Add del old cred

--- main/gsc_inspect_url/inspect_url.py
+++ main/gsc_inspect_url/inspect_url.py
@@ -18,6 +18,9 @@ def start_inspect(self, token, sheets_url):
 class GscIndexInspect:
     def __init__(self, sheets_url, token):
         self.token = token
+        # save sheets url to db
+        InspectSheetsURL(inspect_cred=CredentialsModel.objects.get(token=token),
+                         sheets_url=sheets_url).save()
         self.sheets_token = match('https:\/\/docs.google.com\/spreadsheets\/d\/([^\/]+)\/.*', sheets_url).groups()[0]
         self.authorization_gsc()
         self.authorization_sheets()
@@ -34,6 +37,12 @@ class GscIndexInspect:
         self.colA = self.rows.col_values(1)
 
     def authorization_gsc(self):
+        # saving credential to db it's async so we need wait and check whether the data has already been saved
+        while True:
+            if CredentialsModel.objects.get(token=self.token).credential:
+                break
+            else:
+                sleep(1)
         credentials_dict = loads(CredentialsModel.objects.get(token=self.token).credential)
 
         credentials_ = credentials.Credentials(
@@ -59,6 +68,7 @@ class GscIndexInspect:
 
     def get_url_from_sheets_and_save_to_sheets(self):
         row_num = 1
+        # named first row
         self.rows.update_cell(row_num, 1, "URL")
         self.rows.update_cell(row_num, 2, "Coverage state")
         self.rows.update_cell(row_num, 3, "Indexing state")
@@ -71,62 +81,65 @@ class GscIndexInspect:
         self.rows.update_cell(row_num, 10, "Mobile usability result")
 
         row_num = 2
-        for url in self.colA:
-            if match('^http.*', url):
-                try:
-                    domain = match(r'(https?:\/\/[^\/]+\/)', url)
-                    response = self.index_inspect(url, domain.groups()[0])
-                except:
-                    response = {'inspectionResult':
-                                    {'indexStatusResult': {'coverageState': 'ERROR', 'indexingState': 'ERROR'}}}
-                try:
-                    coverage_state = response['inspectionResult']['indexStatusResult']['coverageState']
-                except KeyError:
-                    coverage_state = "No data"
-                try:
-                    indexing_state = response['inspectionResult']['indexStatusResult']['indexingState']
-                except KeyError:
-                    indexing_state = "No data"
-                try:
-                    last_crawl_time = response['inspectionResult']['indexStatusResult']['lastCrawlTime']
-                except KeyError:
-                    last_crawl_time = "No data"
-                try:
-                    page_fetch_state = response['inspectionResult']['indexStatusResult']['pageFetchState']
-                except KeyError:
-                    page_fetch_state = "No data"
-                try:
-                    google_canonical = response['inspectionResult']['indexStatusResult']['googleCanonical']
-                except KeyError:
-                    google_canonical = "No data"
-                try:
-                    user_canonical = response['inspectionResult']['indexStatusResult']['userCanonical']
-                except KeyError:
-                    user_canonical = "No data"
-                try:
-                    crawled_as = response['inspectionResult']['indexStatusResult']['crawledAs'] + ' bot'
-                except KeyError:
-                    crawled_as = "No data"
-                try:
-                    detected_micro_data = ''
-                    for i in response['inspectionResult']['richResultsResult']['detectedItems']:
-                        detected_micro_data += str(i)+'\n'
-                except KeyError:
-                    detected_micro_data = "No data"
+        for url in self.colA[1:]:
+            # sometimes someone add url without start as http
+            if not match('^http.*', url):
+                url = 'https://'+url
 
-                try:
-                    mobile_usability_result = str(response['inspectionResult']['mobileUsabilityResult'])
-                except KeyError:
-                    mobile_usability_result = "No data"
+            try:
+                domain = match(r'(https?:\/\/[^\/]+\/)', url)
+                response = self.index_inspect(url, domain.groups()[0])
+            except:
+                response = {'inspectionResult':
+                                {'indexStatusResult': {'coverageState': 'ERROR', 'indexingState': 'ERROR'}}}
+            try:
+                coverage_state = response['inspectionResult']['indexStatusResult']['coverageState']
+            except KeyError:
+                coverage_state = "No data"
+            try:
+                indexing_state = response['inspectionResult']['indexStatusResult']['indexingState']
+            except KeyError:
+                indexing_state = "No data"
+            try:
+                last_crawl_time = response['inspectionResult']['indexStatusResult']['lastCrawlTime']
+            except KeyError:
+                last_crawl_time = "No data"
+            try:
+                page_fetch_state = response['inspectionResult']['indexStatusResult']['pageFetchState']
+            except KeyError:
+                page_fetch_state = "No data"
+            try:
+                google_canonical = response['inspectionResult']['indexStatusResult']['googleCanonical']
+            except KeyError:
+                google_canonical = "No data"
+            try:
+                user_canonical = str(response['inspectionResult']['indexStatusResult']['userCanonical'])
+            except KeyError:
+                user_canonical = "No data"
+            try:
+                crawled_as = response['inspectionResult']['indexStatusResult']['crawledAs'] + ' bot'
+            except KeyError:
+                crawled_as = "No data"
+            try:
+                detected_micro_data = ''
+                for i in response['inspectionResult']['richResultsResult']['detectedItems']:
+                    detected_micro_data += str(i)+'\n'
+            except KeyError:
+                detected_micro_data = "No data"
 
-                self.rows.update_cell(row_num, 2, coverage_state)
-                self.rows.update_cell(row_num, 3, indexing_state)
-                self.rows.update_cell(row_num, 4, last_crawl_time)
-                self.rows.update_cell(row_num, 5, page_fetch_state)
-                self.rows.update_cell(row_num, 6, google_canonical)
-                self.rows.update_cell(row_num, 7, user_canonical)
-                self.rows.update_cell(row_num, 8, crawled_as)
-                self.rows.update_cell(row_num, 9, detected_micro_data)
-                self.rows.update_cell(row_num, 10, mobile_usability_result)
-                row_num += 1
+            try:
+                mobile_usability_result = str(response['inspectionResult']['mobileUsabilityResult'])
+            except KeyError:
+                mobile_usability_result = "No data"
+
+            self.rows.update_cell(row_num, 2, coverage_state)
+            self.rows.update_cell(row_num, 3, indexing_state)
+            self.rows.update_cell(row_num, 4, last_crawl_time)
+            self.rows.update_cell(row_num, 5, page_fetch_state)
+            self.rows.update_cell(row_num, 6, google_canonical)
+            self.rows.update_cell(row_num, 7, user_canonical)
+            self.rows.update_cell(row_num, 8, crawled_as)
+            self.rows.update_cell(row_num, 9, detected_micro_data)
+            self.rows.update_cell(row_num, 10, mobile_usability_result)
+            row_num += 1
         CredentialsModel.objects.filter(token=self.token).delete()
--- main/views_inspect_url.py
+++ main/views_inspect_url.py
@@ -5,7 +5,7 @@ import google_auth_oauthlib.flow
 from secrets import token_urlsafe
 from .gsc_inspect_url.inspect_url import start_inspect
 from json import dumps
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.urls import resolve
 
 icea_flow = google_auth_oauthlib.flow.Flow.from_client_config(cred_icea,
@@ -16,28 +16,15 @@ normal_flow = google_auth_oauthlib.flow.Flow.from_client_config(cred,
             scopes='https://www.googleapis.com/auth/webmasters.readonly')
 normal_redirect_uri = "https://robie-seo.herokuapp.com/inspect-gsc/auth"
 
-def inspect_gsc(response):
-
-    current_url = resolve(response.path_info).url_name
-
-    if 'inspect-gsc-icea' == current_url:
-        flow = icea_flow
-        flow.redirect_uri = icea_redirect_uri
-    else:
-        flow = normal_flow
-        flow.redirect_uri = normal_redirect_uri
-
-    authorization_url, _ = flow.authorization_url(
-        access_type='offline',
-        include_granted_scope='true',
-        prompt='consent')
-    return render(response, f'main/auth.html', {'authorize_url': authorization_url})
+def del_old_cred():
+    # free heroku server only works when someone is using it, so only then i can delete old files
+    for i in CredentialsModel.objects.all():
+        if i.start_date + timedelta(days=1) < datetime.now():
+            i.delete()
 
 
-def inspect_gsc_auth(response):
-    token = token_urlsafe(nbytes=32)
-
-    current_url = resolve(response.path_info).url_name
+@shared_task(bind=True)
+def save_credentials_to_db(self, current_url, authorization_response, token):
 
     if 'inspect-gsc-icea-auth' == current_url:
         flow = icea_flow
@@ -46,7 +33,6 @@ def inspect_gsc_auth(response):
         flow = normal_flow
         flow.redirect_uri = normal_redirect_uri
 
-    authorization_response = response.build_absolute_uri()
     flow.fetch_token(authorization_response=authorization_response)
     credentials = flow.credentials
     credentials_dict = {
@@ -61,12 +47,46 @@ def inspect_gsc_auth(response):
     }
 
     CredentialsModel(token=token,
-                     credential=dumps(credentials_dict)).save()
+                     credential=dumps(credentials_dict),
+                     start_date=datetime.now()).save()
+    # free heroku server only works when someone is using it, so only then i can delete old files
+    del_old_cred()
+
+
+def inspect_gsc(response):
+
+    current_url = resolve(response.path_info).url_name
+
+    if 'inspect-gsc-icea' == current_url:
+        flow = icea_flow
+        flow.redirect_uri = icea_redirect_uri
+    else:
+        flow = normal_flow
+        flow.redirect_uri = normal_redirect_uri
+
+    # get auth url
+    authorization_url, _ = flow.authorization_url(
+        access_type='offline',
+        include_granted_scope='true',
+        prompt='consent')
+    return render(response, f'main/auth.html', {'authorize_url': authorization_url})
+
+
+def inspect_gsc_auth(response):
+    token = token_urlsafe(nbytes=32)
+
+    # Saving credentials on free heroku version it's slow and user's get 500 so I needed to add async function
+    current_url = resolve(response.path_info).url_name
+    authorization_response = response.build_absolute_uri()
+    save_credentials_to_db.delay(current_url=current_url,
+                                 authorization_response=authorization_response,
+                                 token=token)
 
     return HttpResponseRedirect(f'/inspect-gsc/auth/{token}')
 
 
 def inspect_gsc_auth_start(response, token):
+    # render page after success auth
     return render(response, f'main/inspect_gsc_auth.html', {'token': token})
 
 
@@ -75,6 +95,8 @@ def auth_start_inspect(response):
     if is_ajax and response.method == "POST":
         token = response.POST['token']
         sheets_url = response.POST['sheets_url']
+
+        # start inspect
         start_inspect.delay(token=token, sheets_url=sheets_url)
         return JsonResponse({"Success": "success", "token": token, "sheets_url": sheets_url})
     return JsonResponse({"Error": "error"})
